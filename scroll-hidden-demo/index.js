013b0d6af9928a61a0898b066c3bbea4b7bc1d57
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-01-06 18:57:03 +0200

    add solution and description of the problem

--- scroll-hidden-demo/index.js
+++ scroll-hidden-demo/index.js
@@ -1,1 +1,36 @@
-console.log('Hello World!')
+// get all lis 
+const lis = document.querySelectorAll('li');
+// get source btn that will be cloned
+const sourceBtn = document.querySelector('.delete-btn');
+// get wrapper, so we can disable its scroll
+const wrapper = document.querySelector('.wrapper');
+// get container, so we can add padding of scrollbar when it is removed
+const container = document.querySelector('.container');
+
+document.body.overflow = '';
+
+// loop through lis
+for (let li of lis) {
+    const span = document.createElement('span');
+
+    // clone sourceBtn, attach event listener, reset its display, so it's visiable and append it to li.
+    const button = sourceBtn.cloneNode(true);
+    button.addEventListener('click', () => handler(li));
+    button.style.display = 'block';
+    li.append(button)
+}
+
+function handler(item) {
+    // get scrollbar width
+    const scrollbarWidth = document.body.clientWidth - document.body.offsetWidth;
+    // add the scrollbar width as padding-right to ul container; 
+    // this is the fix of the content flick problem
+    container.style.paddingRight = `${scrollbarWidth}px`;
+    // hide scrollbar
+    wrapper.style.overflow = 'hidden';
+
+    // delay alert so overflow:hidden can be applied and the content flick be seen;
+    setTimeout(() => {
+        alert('Are you sure you want to delete this image?');
+    }, 100)
+}
