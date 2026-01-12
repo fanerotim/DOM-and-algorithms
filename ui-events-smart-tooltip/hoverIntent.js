8e9368eb93df4bcd9c109ba0c5373ba758fb33c2
fanerotim <61364731+fanerotim@users.noreply.github.com>
2026-01-12 23:48:30 +0200

    seems to be an issue with my formula for speed. when i updated it one test passed and now I am left with one last failing test

--- ui-events-smart-tooltip/hoverIntent.js
+++ ui-events-smart-tooltip/hoverIntent.js
@@ -34,14 +34,36 @@ class HoverIntent {
 
     onMouseOver(event) {
         /* ... */
-        const positionX = event.clientX;
-        const positionY = event.clientY;
+        this.positionX = event.clientX;
+        this.positionY = event.clientY;
+
+        elem.addEventListener('mousemove', this.onMouseMove);
+
+        // when I calculate speed with the below formula, then one of the failing tests passes
+        let speed = 0;
+
+        speed = Math.sqrt(
+            Math.pow(this.positionX - event.clientX, 2) +
+            Math.pow(this.positionY - event.clientY, 2)
+        );
+
+        console.log(speed)
 
         this.intervalId = setInterval(() => {
-            if (Math.abs(positionX - event.clientX < this.sensitivity) || Math.abs(positionY - event.clientY) < this.sensitivity) {
+
+            // one test was failing due to my calculations below
+
+            // if (
+            //     Math.abs(this.positionX - event.clientX < this.sensitivity) ||
+            //     Math.abs(this.positionY - event.clientY) < this.sensitivity) {
+            //     this.over();
+            // }
+
+            // this is from the formula with speed variable. one test still fails
+            if (speed < this.sensitivity) {
                 this.over();
             }
-        }, this.interval)
+        }, this.intervalId)
     }
 
     onMouseOut(event) {
@@ -52,12 +74,16 @@ class HoverIntent {
 
     onMouseMove(event) {
         /* ... */
+        document.querySelector('#tooltip').hidden = 'true';
     }
 
 
     destroy() {
         /* your code to "disable" the functionality, remove all handlers */
         /* it's needed for the tests to work */
+        this.elem.removeEventListener('mousemove', this.onMouseMove);
+        this.elem.removeEventListener('mouseover', this.onMouseOver);
+        this.elem.removeEventListener('mouseout', this.onMouseOut);
     }
 
 }
